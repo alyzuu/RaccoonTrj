12ed43fd82bcaa0c4e5dae0ae6cdf97052775596
purple_racc00n <purple_rac00n@localhost.localdomain>
2016-09-05 17:28:52 +0200

    change

--- modules/environment.py
+++ modules/environment.py
@@ -1,5 +1,13 @@
-import os
 
 def run(**args):
-	print "[*] In environment module."
-	return str(os.environ), "envr"
+	try:
+		import os
+	except:
+		print "Could not load the module on this host. Skipping."
+		return 0, "failed"
+	try:
+		print "[*] In environment module."
+		return str(os.environ), "envr"
+	except:
+		print "Could not run module, exiting..."
+		return 0, "failed"

